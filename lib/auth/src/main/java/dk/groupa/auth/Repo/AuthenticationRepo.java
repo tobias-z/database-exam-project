112d7b5c78b619f096abd99b248fa6dcb8325863
Bryde ツ <6480864+mbryde@users.noreply.github.com>
2023-05-10 10:43:46 +0200

    Calling StoredProcedure to create user
    Using a JPA customer query to call a stored procedure to create a new user.

--- lib/auth/src/main/java/dk/groupa/auth/Repo/AuthenticationRepo.java
+++ lib/auth/src/main/java/dk/groupa/auth/Repo/AuthenticationRepo.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AuthenticationRepo extends JpaRepository<User, Long> {
     @Query(value = "SELECT * FROM dbo.[user] WHERE email = :email AND password = :password", nativeQuery = true)
     Optional<User> findUserByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+
+    @Query(value = "EXEC sp_CreateUser @email = :email, @password = :password, @role = :role", nativeQuery = true)
+    User createUser(@Param("email") String email, @Param("password") String password, @Param("role") String role);
 }

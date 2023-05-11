e513516cc947a3937fa205ed2c874cc0549bdf99
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-05-11 17:49:28 +0200

    feat: load config file using serde-yaml

--- hack/init-conf/src/main.rs
+++ hack/init-conf/src/main.rs
@@ -1,6 +1,7 @@
-use std::{fs, thread, time::Duration};
+use std::{fs, thread};
 
 use pub_sub::{PubSub, Subscription};
+use serde::Deserialize;
 
 // read directory names
 // start process for each of them
@@ -44,12 +45,49 @@ struct ConfigProcess<'r> {
     rx: Subscription<&'r str>,
 }
 
+#[derive(Deserialize)]
+#[serde(rename_all = "kebab-case")]
+struct Config {
+    depends_on: Option<Vec<String>>,
+    after_start: Option<String>,
+}
+
 impl<'r> ConfigProcess<'r> {
     fn new(name: String, channel: PubSub<&'r str>, rx: Subscription<&'r str>) -> Self {
         Self { name, channel, rx }
     }
 
-    fn process_config(&self) {
+    pub fn process_config(&self) {
+        let config = self.load_config();
+        if let Some(config) = &config {
+            self.pre_deploy(config);
+        }
+        Self::deploy();
+        if let Some(config) = &config {
+            self.post_deploy(config);
+        }
         println!("completed config for {}", self.name);
+    }
+
+    fn load_config(&self) -> Option<Config> {
+        match fs::read_to_string(format!("conf/{}/config.yml", self.name)) {
+            Ok(config) => serde_yaml::from_str(&config).ok(),
+            Err(err) => {
+                eprintln!("Error in config file for config {}: {}", self.name, err);
+                None
+            }
+        }
+    }
+
+    fn pre_deploy(&self, config: &Config) {
+        todo!()
+    }
+
+    fn post_deploy(&self, config: &Config) {
+        todo!()
+    }
+
+    fn deploy() {
+        todo!()
     }
 }

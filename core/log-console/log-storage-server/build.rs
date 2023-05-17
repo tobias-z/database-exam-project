556a90845478eef35e63fcdcb05561a34a2d1fb5
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-05-17 13:30:07 +0200

    feat: add experimental optional feature

--- core/log-console/log-storage-server/build.rs
+++ core/log-console/log-storage-server/build.rs
@@ -2,6 +2,8 @@ use std::io;
 
 pub fn main() -> io::Result<()> {
     tonic_build::compile_protos("../contracts/logging-contract.proto")?;
-    tonic_build::compile_protos("../../../lib/auth-proto/src/main/proto/schema.proto")?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&["../../../lib/auth-proto/src/main/proto/schema.proto"], &["../../../lib/auth-proto/src/main/proto"])?;
     Ok(())
 }

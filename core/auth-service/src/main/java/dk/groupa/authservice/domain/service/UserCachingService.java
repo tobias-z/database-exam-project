57bc9d2e5275aef7acb8e543c327d8e53552fc28
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-05-17 12:57:09 +0200

    feat: add ttl on the role emails

--- core/auth-service/src/main/java/dk/groupa/authservice/domain/service/UserCachingService.java
+++ core/auth-service/src/main/java/dk/groupa/authservice/domain/service/UserCachingService.java
@@ -1,5 +1,6 @@
 package dk.groupa.authservice.domain.service;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
@@ -10,14 +11,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserCachingService {
 
+    // not sure what is the best timeout here tbh
+    private static final Duration ROLE_TIMEOUT = Duration.ofDays(7);
+
     private final RedisTemplate<String, String> redisTemplate;
 
     public Set<String> getEmailsOfRole(String role) {
-        return redisTemplate.opsForSet().members(role);
+        return this.redisTemplate.opsForSet().members(role);
     }
 
     public void saveEmailsInRole(String role, List<String> emails) {
         this.redisTemplate.opsForSet().add(role, emails.toArray(new String[0]));
+        this.redisTemplate.expire(role, ROLE_TIMEOUT);
     }
 
 }

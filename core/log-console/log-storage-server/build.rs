ab64880d76020de68068c671e78af93df40bbb52
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-05-18 16:50:22 +0200

    fix: copy instead of symlink

--- core/log-console/log-storage-server/build.rs
+++ core/log-console/log-storage-server/build.rs
@@ -4,6 +4,6 @@ pub fn main() -> io::Result<()> {
     tonic_build::compile_protos("../contracts/logging-contract.proto")?;
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(&["../contracts/schema.proto"], &["../contracts"])?;
+        .compile(&["../contracts/auth.proto"], &["../contracts"])?;
     Ok(())
 }

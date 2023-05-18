891fe5a83f962fc41d0317343625d07b717fe14b
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-05-18 16:23:05 +0200

    fix: make symlink to the schema so that we can use it in docker build

--- core/log-console/log-storage-server/build.rs
+++ core/log-console/log-storage-server/build.rs
@@ -5,8 +5,8 @@ pub fn main() -> io::Result<()> {
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile(
-            &["../../../lib/auth-proto/src/main/proto/schema.proto"],
-            &["../../../lib/auth-proto/src/main/proto"],
+            &["../contracts/schema.proto"],
+            &["../contracts"],
         )?;
     Ok(())
 }

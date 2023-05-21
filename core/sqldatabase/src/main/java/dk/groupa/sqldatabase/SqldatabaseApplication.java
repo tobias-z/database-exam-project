245e78091331975c1e22266a2a56295becb17483
Mathias Jensen <mathias@Mathiass-MacBook-Pro.local>
2023-05-21 10:56:27 +0200

    auth lib added

--- core/sqldatabase/src/main/java/dk/groupa/sqldatabase/SqldatabaseApplication.java
+++ core/sqldatabase/src/main/java/dk/groupa/sqldatabase/SqldatabaseApplication.java
@@ -1,7 +1,12 @@
 package dk.groupa.sqldatabase;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@ComponentScan({
+		"dk.groupa"
+})
 
 @SpringBootApplication
 public class SqldatabaseApplication {

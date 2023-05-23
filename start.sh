2dabbd3cb47b5ce3d6dc9d4f585a6adf11a408db
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-05-23 10:28:39 +0200

    fix: change config to use open ports

--- start.sh
+++ start.sh
@@ -30,7 +30,7 @@ if [ -z $version ]; then
     echo "No version provided. Using the latest."
     if docker image ls | grep "tobiaszimmer/root-app" | grep latest; then
         echo "Found old latest version. Deleting to ensure we use the latest."
-        docker rm tobiaszimmer/root-app:latest
+        docker image rm tobiaszimmer/root-app:latest
         if [ $? -eq 1 ]; then
             echo "Unable to remove old latest version of 'tobiaszimmer/root-app:latest'. Perhaps you need to stop an already running one."
             exit 1
